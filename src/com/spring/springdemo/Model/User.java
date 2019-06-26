89115125d4c8c41cee0cd006f0e19eb8696c9811
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-06-26 15:41:23 +0300

    changed user to entity

--- src/com/spring/springdemo/Model/User.java
+++ src/com/spring/springdemo/Model/User.java
@@ -1,16 +1,28 @@
 package com.spring.springdemo.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @Column(name = "id")
+    private int id;
 
     @NotNull(message = "*is required")
     @Size(min = 1, message = "*is required")
+    @Column(name = "username")
     private String username;
 
     @NotNull(message = "*is required")
     @Size(min = 1, message = "*is required")
+    @Column(name = "password")
     private String password;
 
     public User() {
@@ -34,5 +46,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
