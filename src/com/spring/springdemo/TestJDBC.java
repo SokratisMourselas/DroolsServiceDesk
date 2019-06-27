3af23103c625ffccc83f39887da9649f9463c1de
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-06-27 12:44:30 +0300

    testing db

--- src/com/spring/springdemo/TestJDBC.java
+++ src/com/spring/springdemo/TestJDBC.java
@@ -5,32 +5,51 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.NoResultException;
+
 public class TestJDBC {
 
     public static void main(String[] args) {
 
+        System.out.println("\nConnecting to Database");
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class).buildSessionFactory();
+        System.out.println("<<Connection successful>>\n");
 
         Session session = factory.getCurrentSession();
 
         try {
-            System.out.println("\nConnecting to Database");
-
+            // Creating a dummy User
             User newUser = new User();
-            newUser.setUsername("Sokratis");
+            newUser.setUsername("Sokratis MOURSELAS");
             newUser.setPassword("ExamplePassword");
             newUser.setEmail("example@springMVC.com");
+
+            //Beginning Transaction
             session.beginTransaction();
+
+            //Saving the Dummy User
             session.save(newUser);
 
-            System.out.println("\nRetrieving a User: ");
+            newUser.setPassword("ChangedPassword");
 
-            User userFromDb = session.get(User.class, 1);
+
+            // Retrieving and checking if the User was created
+            System.out.println("\n<<Retrieving a Dummy User>>");
+            User userFromDb = new User();
+
+            String customHQLQuery = "from User u where u.username LIKE '%MOURSELAS'";
+
+            try {
+                userFromDb =(User) session.createQuery(customHQLQuery).getSingleResult();
+            } catch (NoResultException n){
+
+            }
+
+            System.out.println("Dummy User successfully retrieved: \n"+ userFromDb);
+
+            // Truncating table
+            session.delete(newUser);
             session.getTransaction().commit();
-
-            System.out.println("<<Connection successful>>");
-
-            System.out.println(userFromDb);
 
         } catch (Exception e){
             e.printStackTrace();
