b296cbd978fb3c07b0de73deafc8b646a139bf4d
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-07-05 17:15:44 +0300

    added many to many relationship

--- src/com/spring/springdemo/Model/Comment.java
+++ src/com/spring/springdemo/Model/Comment.java
@@ -1,6 +1,7 @@
 package com.spring.springdemo.Model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "comment")
@@ -10,6 +11,14 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "comment_user",
+            joinColumns = @JoinColumn(name = "commentId"),
+            inverseJoinColumns = @JoinColumn(name = "userId")
+    )
+    private List<User> userList;
 
     @Column(name = "comment")
     private String comment;
