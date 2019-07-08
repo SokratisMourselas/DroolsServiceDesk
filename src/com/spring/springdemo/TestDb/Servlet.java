3163463e6d777210d775c577b43ff15ba1ba8306
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-07-08 13:34:35 +0300

    added servlet connection

---
+++ src/com/spring/springdemo/TestDb/Servlet.java
@@ -0,0 +1,41 @@
+package com.spring.springdemo.TestDb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@WebServlet("/TestDbServlet")
+public class Servlet extends javax.servlet.http.HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String user = "root";
+        String password = "root";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/springdrools?useSSL=false&serverTimezone=UTC";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        //getting connection
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("Connecting to: "+ jdbcUrl);
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+            out.println("Connection Successful!!!!");
+            myConn.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+    }
+}
