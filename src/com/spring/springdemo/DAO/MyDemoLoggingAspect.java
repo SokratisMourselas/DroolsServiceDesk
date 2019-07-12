9abb50185e25757e51b7dd1e21f89c6a8c11ff93
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-07-12 10:41:40 +0300

    added pointcut

--- src/com/spring/springdemo/DAO/MyDemoLoggingAspect.java
+++ src/com/spring/springdemo/DAO/MyDemoLoggingAspect.java
@@ -2,15 +2,24 @@ package com.spring.springdemo.DAO;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(* com.spring.springdemo.DAO.*DAO.add*())")
+    @Pointcut("execution(* com.spring.springdemo.DAO.*DAO.add*())")
+    private void forDAOPackage(){}
+
+    @Before("forDAOPackage()")
     public void beforeAddUserAdvice() {
-        System.out.println("Executing @Before on add*()");
+        System.out.println("\n   =======   >>>>> Executing @Before on add*()");
+    }
+
+    @Before("forDAOPackage()")
+    public void printAnalytics(){
+        System.out.println("   =======   >>>>> Performing some API Analytics");
     }
 
 }
